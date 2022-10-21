c3d8e59725d85560490376194937346f9ec30d8b
Eugene Baraka <eugenebaraka@gmail.com>
2022-10-20 23:16:59 -0400

    new exercises

---
+++ python3hardway/exercise3.py
@@ -0,0 +1,15 @@
+print("I will now count my chickens:")
+
+print("hens", 23+5 /5)
+print("Roosters", 109-88*99)
+
+print(33-4 <44)
+
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+print("Oh, that's why it's False.")
+
+print("How about some more.") 
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
