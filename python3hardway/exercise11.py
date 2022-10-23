23762964bd89a2d7b12af78ecfe2a2e7ce004546
Eugene Baraka <eugenebaraka@gmail.com>
2022-10-22 22:05:39 -0400

    new execs

---
+++ python3hardway/exercise11.py
@@ -0,0 +1,10 @@
+print("How old are you?", end=' ')
+age = input(10)
+
+print("How tall are you?", end=' ')
+height = input(130)
+
+print("How much do you weigh?", end=' ')
+weight = input(25)
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy.")
